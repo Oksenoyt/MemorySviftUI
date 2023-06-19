ad897d2f0e486fa42eedc0254ea47b8a54191c0a
Oksenoyt <a.oksenoyt@gmail.com>
2023-06-18 18:10:32 -0700

    Initial Commit

---
+++ MemorySviftUI/MemorySviftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorySviftUIApp.swift
+//  MemorySviftUI
+//
+//  Created by Elenka on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorySviftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
