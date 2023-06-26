eddae7309452f237d4b8dfa04546e78d90f31645
Oksenoyt <a.oksenoyt@gmail.com>
2023-06-26 15:36:30 -0700

    fix

--- MemorySviftUI/EmojiMemoryGameView.swift
+++ MemorySviftUI/EmojiMemoryGameView.swift
@@ -37,18 +37,30 @@ struct CardView: View {
     let card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else {
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrowingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape.strokeBorder(lineWidth: DrowingConstants.lineWidth)
+                    Text(card.content)
+                        .font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
             }
         }
+    }
+
+    private func font(in size: CGSize) -> Font {
+        .system(size: min(size.width, size.height) * DrowingConstants.fontScale)
+    }
+
+    private struct DrowingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
